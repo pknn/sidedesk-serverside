5fecd517a798b71d995874bed04f58cc243626ba
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-16 20:52:49 +0700

    added typeorm to the project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import Consola from 'consola'
 
 import { getExpressApplication } from 'app/applications'
+import { initializeTypeOrm } from './applications/typeorm'
 
 const host = process.env.HOST || 'localhost'
 const port = parseInt(process.env.PORT || '9000')
 
 export const application = getExpressApplication()
+initializeTypeOrm()
 
 application.listen(port, host, () => {
   Consola.start(
