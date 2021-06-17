809cd9da9a32a2dbc37ea4aac5733ea2022816d3
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-17 17:03:37 +0700

    edited Ticket entity mock

--- tests/Ticket/entity.mock.ts
+++ tests/Ticket/entity.mock.ts
@@ -2,10 +2,10 @@ import faker from 'faker'
 
 import { Ticket } from '../../src/entities/Ticket'
 
-export const getMockTicket = (includeEmail: boolean = true): Ticket =>
+export const getMockTicket = (): Ticket =>
   new Ticket(
     faker.lorem.sentence(5),
     faker.lorem.sentences(50),
     faker.name.firstName(),
-    includeEmail ? faker.internet.email() : undefined,
+    faker.internet.email(),
   )
