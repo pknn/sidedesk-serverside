87f2c799186ffad46b5584ef89971262e7068047
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-17 16:25:48 +0700

    edited Ticket entity test to include only one constructor test

--- tests/Ticket/entity.spec.ts
+++ tests/Ticket/entity.spec.ts
@@ -6,52 +6,27 @@ import { getMockTicket } from './entity.mock'
 
 describe('Ticket Entity', () => {
   describe('contructor()', () => {
-    describe('when reporterEmail is defined', () => {
-      it('should get new Ticket entity with correct information', () => {
-        const mockTicket = getMockTicket()
-        const ticket = new Ticket(
-          mockTicket.title,
-          mockTicket.description,
-          mockTicket.reporterName,
-          mockTicket.reporterEmail,
-        )
+    it('should get new Ticket entity with correct information', () => {
+      const mockTicket = getMockTicket()
+      const ticket = new Ticket(
+        mockTicket.title,
+        mockTicket.description,
+        mockTicket.reporterName,
+        mockTicket.reporterEmail,
+      )
 
-        expect(ticket).to.be.an('object')
-        expect(ticket).to.have.property('id')
-        expect(ticket.title).to.be.a('string')
-        expect(ticket.title).to.equal(mockTicket.title)
-        expect(ticket.description).to.be.a('string')
-        expect(ticket.description).to.equal(mockTicket.description)
-        expect(ticket.reporterName).to.be.a('string')
-        expect(ticket.reporterName).to.equal(mockTicket.reporterName)
-        expect(ticket.reporterEmail).to.be.a('string')
-        expect(ticket.reporterEmail).to.equal(mockTicket.reporterEmail)
-        expect(ticket).to.have.property('createdAt')
-        expect(ticket).to.have.property('updatedAt')
-      })
-    })
-    describe('when reporterEmail is NOT defined', () => {
-      it('should get new Ticket entity with correct information', () => {
-        const mockTicket = getMockTicket(false)
-        const ticket = new Ticket(
-          mockTicket.title,
-          mockTicket.description,
-          mockTicket.reporterName,
-        )
-
-        expect(ticket).to.be.an('object')
-        expect(ticket).to.have.property('id')
-        expect(ticket.title).to.be.a('string')
-        expect(ticket.title).to.equal(mockTicket.title)
-        expect(ticket.description).to.be.a('string')
-        expect(ticket.description).to.equal(mockTicket.description)
-        expect(ticket.reporterName).to.be.a('string')
-        expect(ticket.reporterName).to.equal(mockTicket.reporterName)
-        expect(ticket.reporterEmail).to.be.an('undefined')
-        expect(ticket.reporterEmail).to.equal(mockTicket.reporterEmail)
-        expect(ticket).to.have.property('createdAt')
-        expect(ticket).to.have.property('updatedAt')
-      })
+      expect(ticket).to.be.an('object')
+      expect(ticket).to.have.property('id')
+      expect(ticket.title).to.be.a('string')
+      expect(ticket.title).to.equal(mockTicket.title)
+      expect(ticket.description).to.be.a('string')
+      expect(ticket.description).to.equal(mockTicket.description)
+      expect(ticket.reporterName).to.be.a('string')
+      expect(ticket.reporterName).to.equal(mockTicket.reporterName)
+      expect(ticket.reporterEmail).to.be.a('string')
+      expect(ticket.reporterEmail).to.equal(mockTicket.reporterEmail)
+      expect(ticket).to.have.property('createdAt')
+      expect(ticket).to.have.property('updatedAt')
     })
   })
 })
