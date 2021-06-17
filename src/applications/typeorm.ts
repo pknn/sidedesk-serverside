f27e2227c2e85e539bf0c27b0dec95c359b5bb9b
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-17 17:03:10 +0700

    edited configuration for test database

--- src/applications/typeorm.ts
+++ src/applications/typeorm.ts
@@ -1,5 +1,13 @@
 import { createConnection } from 'typeorm'
 
 export const initializeTypeOrm = async () => {
-  await createConnection(process.env.NODE_ENV === 'test' ? 'test' : 'default')
+  process.env.NODE_ENV === 'test'
+    ? await createConnection({
+        name: 'default',
+        type: 'sqlite',
+        database: './test.db',
+        entities: ['dist/entities/*.js', 'src/entities/*.ts'],
+        synchronize: true,
+      })
+    : await createConnection()
 }
