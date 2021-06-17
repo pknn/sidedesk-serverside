9335c994bc0dccb103ec0f8d0f0ef30e8863f8e2
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-17 19:47:33 +0700

    added Ticket Use Case implementation

---
+++ src/types/TicketQueryOptions.ts
@@ -0,0 +1,15 @@
+import { TicketStatus } from './TicketStatus'
+
+export type TicketSortingColumn = 'status' | 'created_at'
+export type TicketSortingStrategy = 'ASC' | 'DESC'
+export interface TicketSortingOption {
+  sortBy: TicketSortingColumn
+  strategy: TicketSortingStrategy
+}
+
+export interface TicketQueryOption {
+  offset?: number
+  limit?: number
+  sorting?: TicketSortingOption
+  status?: TicketStatus
+}
---
+++ src/useCases/Ticket.ts
@@ -0,0 +1,47 @@
+import {
+  TicketCreationJsonBody,
+  TicketUpdateJsonBody,
+  toEntity,
+} from 'app/bodies/Ticket'
+import { Ticket as TicketEntity, toModel } from 'app/entities/Ticket'
+import { Ticket } from 'app/models/Ticket'
+import { TicketQueryOption } from 'app/types/TicketQueryOptions'
+
+export const getTicket = async (id: number): Promise<Ticket> => {
+  const ticket = await TicketEntity.findOneOrFail(id)
+  return toModel(ticket)
+}
+
+export const getTickets = async (
+  overrideOptions?: TicketQueryOption,
+): Promise<Ticket[]> => {
+  const option: TicketQueryOption = {
+    offset: 0,
+    limit: 50,
+    sorting: {
+      sortBy: 'created_at',
+      strategy: 'ASC',
+    },
+    ...overrideOptions,
+  }
+
+  const baseQuery = TicketEntity.createQueryBuilder()
+    .offset(option.offset!)
+    .limit(option.limit)
+    .orderBy(option.sorting!.sortBy, option.sorting!.strategy)
+
+  const finalQuery = option.status
+    ? await baseQuery.where({ status: option.status }).getMany()
+    : await baseQuery.getMany()
+
+  return finalQuery.map(toModel)
+}
+
+export const create = async (
+  ticketCreateJsonBody: TicketCreationJsonBody,
+): Promise<Ticket> => toModel(await toEntity(ticketCreateJsonBody).save())
+
+export const update = async (
+  id: number,
+  ticketUpdateBody: TicketUpdateJsonBody,
+): Promise<any> => await TicketEntity.update(id, ticketUpdateBody)
