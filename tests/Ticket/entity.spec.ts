a36c4f572df62616120effa3c821ac1efa77ac28
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-17 16:32:12 +0700

    added test suite for Ticket entity toModel() method

--- tests/Ticket/entity.spec.ts
+++ tests/Ticket/entity.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 
 // import { initializeTypeOrm } from '../../src/applications/typeorm'
-import { Ticket } from '../../src/entities/Ticket'
+import { Ticket, toModel } from '../../src/entities/Ticket'
 import { getMockTicket } from './entity.mock'
 
 describe('Ticket Entity', () => {
@@ -17,16 +17,41 @@ describe('Ticket Entity', () => {
 
       expect(ticket).to.be.an('object')
       expect(ticket).to.have.property('id')
-      expect(ticket.title).to.be.a('string')
+      expect(ticket).to.have.property('title')
       expect(ticket.title).to.equal(mockTicket.title)
-      expect(ticket.description).to.be.a('string')
+      expect(ticket).to.have.property('descriptiom')
       expect(ticket.description).to.equal(mockTicket.description)
-      expect(ticket.reporterName).to.be.a('string')
+      expect(ticket).to.have.property('reporterName')
       expect(ticket.reporterName).to.equal(mockTicket.reporterName)
-      expect(ticket.reporterEmail).to.be.a('string')
+      expect(ticket).to.have.property('reporterEmail')
       expect(ticket.reporterEmail).to.equal(mockTicket.reporterEmail)
+      expect(ticket).to.have.property('status')
+      expect(ticket.status).to.equal(mockTicket.status)
       expect(ticket).to.have.property('createdAt')
       expect(ticket).to.have.property('updatedAt')
+    })
+  })
+
+  describe('toModel()', () => {
+    it('should return correct Ticket model', () => {
+      const ticketEntity = getMockTicket()
+      const ticketModel = toModel(ticketEntity)
+
+      expect(ticketModel).to.be.an('object')
+      expect(ticketModel).to.have.property('id')
+      expect(ticketModel.id).to.equal(ticketEntity.id)
+      expect(ticketModel).to.have.property('title')
+      expect(ticketModel.title).to.equal(ticketEntity.title)
+      expect(ticketModel).to.have.property('description')
+      expect(ticketModel.description).to.equal(ticketEntity.description)
+      expect(ticketModel).to.have.property('reporterName')
+      expect(ticketModel.reporterName).to.equal(ticketEntity.reporterName)
+      expect(ticketModel).to.have.property('reporterEmail')
+      expect(ticketModel.reporterEmail).to.equal(ticketEntity.reporterEmail)
+      expect(ticketModel).to.have.property('createdAt')
+      expect(ticketModel.createdAt).to.equal(ticketEntity.createdAt)
+      expect(ticketModel).to.have.property('updatedAt')
+      expect(ticketModel.updatedAt).to.equal(ticketEntity.updatedAt)
     })
   })
 })
